286424f735c5e63b39ed64065c7fdcd8771937d9
Mike Zanercik <Michael.zanercik@gmail.com>
2019-10-29 10:03:20 -0400

    finalized the front end display of the page and started working the js

---
+++ assets/javascript.js
@@ -0,0 +1,20 @@
+// var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city;
+// var city = "London"
+
+$(".btn-primary").on("click", function(){
+    var cityName = $("#textInput").text();
+    console.log(cityName)
+})
+
+
+
+
+
+
+// $.ajax({
+//     url: queryUrl,
+//     method: 'GET'
+// }) .then(function(response){
+//     console.log(response)
+// })
+
