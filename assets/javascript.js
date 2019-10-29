9c12a8ff1ecf7de4b7b040c510e693c10c8fb53b
Mike Zanercik <Michael.zanercik@gmail.com>
2019-10-29 14:21:38 -0400

    added simplified js and moment.js file

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,20 +1,31 @@
-// var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city;
-// var city = "London"
 
-$(".btn-primary").on("click", function(){
-    var cityName = $("#textInput").text();
-    console.log(cityName)
+
+$(".btn-primary").on("click", function () {
+    event.preventDefault();
+    city = $(":input.form-control").val();
+
+    var APIKey = "166a433c57516f51dfab1f7edaed8413";
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response)
+        tempF = (((response.main.temp) - 273.15) * 1.80 + 32).toFixed(2)
+        var newList = $("<ul>").addClass("City").text(response.name)
+        var newCityTemp = $("<li>").addClass("list-group-item Temperature").text("Temperature: " + (tempF) + " °F");
+        var newCityHumidity = $("<li>").addClass("list-group-item Humidity").text("Humidity: " + (response.main.humidity) + " %");
+        var newCityWind = $("<li>").addClass("list-group-item Wind").text("Wind Speed: " + (response.wind.speed) + " m/s");
+
+        $("section").append(newList)
+        $("ul").append(newCityTemp, newCityHumidity, newCityWind)
+
+
+    })
 })
 
 
 
 
-
-
-// $.ajax({
-//     url: queryUrl,
-//     method: 'GET'
-// }) .then(function(response){
-//     console.log(response)
-// })
 
